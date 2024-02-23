acce8a112f433c7c006335b7705ef2d795653162
PriyankaMitech <priyanka.mitech@gmail.com>
2024-02-23 15:11:32 +0530

    Login Page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('register', 'Admin_Controller::index');
+$routes->get('login', 'Admin_Controller::index');
 
