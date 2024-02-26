ba71a9d6c1f01cbb0ece051519a2fd5fd1012b3f
PriyankaMitech <priyanka.mitech@gmail.com>
2024-02-26 10:12:58 +0530

    Some Changes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
-// $routes->get('login', 'Admin_Controller::index');
+
 $routes->get('/', 'Admin_Controller::index');
 $routes->get('admin_dashboard', 'Admin_Controller::admin_dashboard');
 $routes->post('login', 'Admin_Controller::login');
--- app/Views/header.php
+++ app/Views/header.php
@@ -2,14 +2,8 @@
 <html lang="en">
 
 <head>
-    <title>GURU Able - Free Lite Admin Template </title>
-    <!-- HTML5 Shim and Respond.js IE9 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
-      <!-- Meta -->
+    <title>Book Appointment</title>
+
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
